7406807bb07c3d33999b6fffbfe99404ba886d27
rfarrell <rfarrell@hubspot.com>
2022-04-05 00:31:40 +0100

    make it better

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Banner () {
     return (
         <div className="banner">
-         <div><h2><i className="far fa-calendar-alt"></i> 90 Days Travelling and Working</h2> </div><img src={process.env.PUBLIC_URL + `/images/ruthlaptop.jpeg`} />
+         <div><h2><i className="far fa-calendar-alt"></i> 90 Days Travelling and Working</h2> 
+        </div>
+         <img src={process.env.PUBLIC_URL + `/images/ruthlaptop.jpeg`} alt="Ruth on Laptop" />
         </div>
     )
 }
